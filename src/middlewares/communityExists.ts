d1ea94c2bc37212c53d6077a5e348362be626600
rramiachraf <onlyjs@tuta.io>
2020-07-23 14:59:19 +0100

    check if community exists middleware

---
+++ src/middlewares/communityExists.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import pool from '../database/index'
+
+const query = `
+    SELECT name FROM main.communities
+    WHERE LOWER(name) = LOWER($1)
+    `
+
+const communityExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { community } = req.params
+  try {
+    const { rowCount } = await pool.query(query, [community])
+    if (rowCount === 0) {
+      throw Error('community not found')
+    }
+    next()
+  } catch ({ message: error }) {
+    res.status(404).send({ error })
+  }
+}
+
+export default communityExists
