71035d6286109b55987cc38a96063f4504e080cb
rramiachraf <onlyjs@tuta.io>
2020-07-24 12:21:47 +0100

    Return community name after deleting it

--- src/routes/community/deleteCommunity.ts
+++ src/routes/community/deleteCommunity.ts
@@ -4,15 +4,16 @@ import pool from '../../database/index'
 const deleteCommunityQuery = `
     DELETE FROM main.communities 
     WHERE LOWER(name) = LOWER($1)
+    RETURNING name
 `
 
 const deleteCommunity = async (req: Request, res: Response) => {
   try {
     const { community } = req.params
-    const query = await pool.query(deleteCommunityQuery, [community])
-    res.send()
-  } catch ({ message }) {
-    res.status(500).send({ message })
+    const { rows } = await pool.query(deleteCommunityQuery, [community])
+    res.send({ message: `${rows[0].name} has been deleted` })
+  } catch ({ message: error }) {
+    res.status(500).send({ error })
   }
 }
 
