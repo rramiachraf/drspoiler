8e88379db89ba9b929fedbcd7338113500f89f3a
rramiachraf <onlyjs@tuta.io>
2020-08-15 18:39:22 +0100

    return username when checking logged user

--- src/routes/user/logged.ts
+++ src/routes/user/logged.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from 'express'
+import pool from '../../database'
+
+const query = `
+  SELECT username FROM main.users
+  WHERE user_id = $1
+`
 
 const logged = async (req: Request, res: Response) => {
-  res.send({ userId: req.session!.userId })
+  try {
+    const { rows } = await pool.query(query, [req.session!.userId])
+    res.send(rows[0])
+  } catch (e) {
+    res.status(404).send()
+  }
 }
 
 export default logged
